173d0c8a5c065cb2c9f1a81d1fd4c98fae504b99
volto9 <volto9@yahoo.com>
2024-11-10 09:19:02 +0000

    first

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import module from 'module';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 });
-module.exports = {
-  root: 'src',
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-  },
-};
